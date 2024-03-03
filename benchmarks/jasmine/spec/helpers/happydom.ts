fa59a0d3a4ab4012ee5f4cf0dfa72157ab460bba
Ev Haus <ev@haus.gg>
2024-03-02 23:28:41 -0800

    Fix broken Jasmine CI runs

--- benchmarks/jasmine/spec/helpers/happydom.ts
+++ benchmarks/jasmine/spec/helpers/happydom.ts
@@ -7,10 +7,17 @@ global.window = window;
 // Register global window extensions
 [
 	'document',
+	// NOTE: This is need for running tests on GitHub Actions CI, but causes
+	// failure on local builds due to this error:
+	// > Cannot set property navigator of #<Object> which has only a getter
+	'navigator',
 	'Element',
 	'getComputedStyle',
 	'HTMLElement',
 	'SVGElement'
 ].forEach((key) => {
-	global[key] = global.window[key];
+	try {
+		global[key] = global.window[key];
+	} catch (e) {
+	}
 });
